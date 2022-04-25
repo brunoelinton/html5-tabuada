513f1d2f6b895ba816b11f050b948416a0a54d44
Bruno Guimarães <brunoelinton@outlook.com>
2022-04-25 19:37:26 -0300

    Project: multiplication table

---
+++ assets/js/tabuada.js
@@ -0,0 +1,37 @@
+/*
+    Exemplo de uma tabuada
+*/
+
+// Declaração da função que realiza o cálcula da tabuada
+function calculaTabuada() {
+    // Obtendo do DOM o tbody da tabela
+    let tabuada = document.querySelector('#tabuada tbody');
+    // Obtendo o valorA do campo input numbere convertendo em inteiro
+    let valorA = parseInt(document.querySelector('#valorA').value);
+    // Limpando o conteúdo do tbody
+    tabuada.innerHTML = '';
+    // Criando um laço de repetição de 0 a 10
+    for (let valorB = 0; valorB <= 10; valorB++) {
+        // Calculando o resultado da linha atual
+        let resultado = valorA * valorB;
+        // Criando os templates das colunas da linha atual
+        let template = `
+            <td>${valorA}</td>
+            <td>x</td>
+            <td>${valorB}</td>
+            <td>=</td>
+            <td>${resultado}</td>
+        `;
+        // Criando o elemento tr
+        let tr = document.createElement('tr');
+        // Inserindo as colunas na linha
+        tr.innerHTML = template;
+        // Inserindo a linha na tabela
+        tabuada.append(tr);
+    } // Finalizando o for
+} // Finalizando a função
+
+// Chamando a função pela primeira vez
+calculaTabuada();
+// Adicionando o evento de alteração ao campo número
+document.querySelector('#valorA').addEventListener('change', calculaTabuada);
